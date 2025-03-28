1de7f71837844939e19d46d5b0a5cac78a82ea96
Beatriz Garcia <beatrizga.pr44@gmail.com>
2025-03-28 18:45:20 +0100

    feat: dibuja una palabra de prueba a medio adivinar

--- sample.py
+++ sample.py
@@ -12,14 +12,19 @@ font_name = pg.font.get_default_font()
 font = pg.font.SysFont(font_name, 70)
 letters_font = pg.font.SysFont(font_name, 60)
 
-
 counter = 0
 status = -1
 playing = True
 
+palabra = 'casa'
+solucion = ''
+
+for letra in palabra:
+    solucion += '-'
+
 # bucle principal
 while playing:
-    clock.tick(20)
+    #clock.tick(20)
     counter += 1
 
     # evento para salir al cerrar la ventana
@@ -32,20 +37,29 @@ while playing:
 
     # cada segundo activa una parte del ahorcado
     # simulando la evolución del juego
-    if counter % 20 == 0:
-        status += 1
-    if counter > 220:
-        status = -1
+    # if counter % 20 == 0:
+    #     status += 1
+    # if counter > 220:
+    #     status = -1
 
     for index, key in enumerate(GRAPHICS):
         if index <= status:
             utils.draw_part(display, key, True, 30)
 
     # dibuja una palabra de prueba a medio adivinar
-    utils.draw_word(display, font, '-ee---d---')
+    
+    # palabra = 'CASA'
+    # palabra = '----'
+
+    utils.draw_word(display, font, solucion)
 
     # dibuja el abecedario con algunas letras probadas
     # erróneas y otras letras válidas
-    utils.draw_letters(display, letters_font, 360, 50, 'amu', 'ed')
+    incorrecto = ''
+    correcto = ''
+    utils.draw_letters(display, letters_font, 360, 50, incorrecto, correcto)
+    #TODO:  Adivinar letras  jugador 2 - Pulsas un letra del teclado
+    #       comprobar si la letra pulsada esta en la palabra que se introdujo, si es asi se pinta en verde ("a") y se 
+    #       muestra en la palabra a adivinar ('-A-A'), sino en rojo ("amu") y se pinta una parte del ahorcado -> status + 1
 
     pg.display.flip()
